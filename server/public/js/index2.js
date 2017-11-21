8960a984ef112f5a38624f05e2b0df6748f7a9af
Terry Yun <nooheat1228@gmail.com>
2017-11-21 15:10:02 +0900

    서버 소스를 다른 레포지토리로 분할합니다. : https://github.com/Nooheat/Lupin-Catcher-Node-Server

--- server/public/js/index2.js
+++
@@ -1,8 +0,0 @@
-function exit() {
-    require('electron').remote.getCurrentWindow().close();
-}
-
-let socket = require('socket.io-client')("http://localhost:5000");
-socket.emit('enter',{
-    "ON": "SEESE"
-});
